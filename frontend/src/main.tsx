c475f433143137cb729e8b8f431414896f13cc64
Goran <temeljkovitch.goran@gmail.com>
2025-01-22 15:28:12 -0400

    feat: add toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
-
+import { Toaster } from "@/components/ui/sonner";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
@@ -21,6 +21,7 @@ createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <Auth0ProviderWithNavigate>
           <AppRoutes />
+          <Toaster visibleToasts={1} position="top-right" richColors/>
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </BrowserRouter>
