61d43f41c988c94eca5bd9e8472318f9b7d9cb6d
Temeljkovitch <temeljkovitch.goran@gmail.com>
2025-01-25 18:21:32 -0300

    deploy: prepare backend for Render hosting

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
@@ -12,7 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/user", userRoute)
+app.get("/health", async (request: Request, response: Response) => {
+  response.send({ message: "Health ok!" });
+});
+
+app.use("/api/v1/user", userRoute);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
